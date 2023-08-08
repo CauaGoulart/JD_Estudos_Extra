f708be6beb6f2d6ebd9e565401012c86501e6ebc
Cauã <114314281+CauaGoulart@users.noreply.github.com>
2023-08-08 14:13:27 -0300

    Add files via upload

--- JPA_Hibernate/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ JPA_Hibernate/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -12,37 +12,52 @@ import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
-	
+
 	public static void main(String[] args) {
 		cadastrarProduto();
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
-		
+
 		Produto p = produtoDao.buscarPorId(1l);
 		System.out.println(p.getPreco());
-		
+
 		List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
 		todos.forEach(p2 -> System.out.println(p.getNome()));
-	
+
 		BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
-		System.out.println("Preco do Produto: " +precoDoProduto);
+		System.out.println("Preco do Produto: " + precoDoProduto);
+
+		List<Produto> todosProdutos = produtoDao.buscarTodos();
+		for (Produto produto : todosProdutos) {
+			System.out.println("ID: " + produto.getId() + " | Nome: " + produto.getNome() + " | Preço: " + produto.getPreco());
+		}
 	}
 
 	private static void cadastrarProduto() {
 		Categoria celulares = new Categoria("CELULARES");
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares );
-		
+		Categoria eletronicos = new Categoria("ELETRÔNICOS");
+		Categoria vestuarios = new Categoria("VESTUÁRIO");
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("900"), celulares);
+		Produto eletronico = new Produto("Torradeita", "Faz torradas", new BigDecimal("80"), eletronicos);
+		Produto vestuario = new Produto("Calça jeans", "Muito bonita", new BigDecimal("65"), vestuarios);
+
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		CategoriaDao categoriaDao = new CategoriaDao(em);
-		
+
 		em.getTransaction().begin();
-		
+
 		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(eletronicos);
+		categoriaDao.cadastrar(vestuarios);
 		produtoDao.cadastrar(celular);
-		
+		produtoDao.cadastrar(eletronico);
+		produtoDao.cadastrar(vestuario);
+
 		em.getTransaction().commit();
 		em.close();
 	}
+
+	
 
 }
